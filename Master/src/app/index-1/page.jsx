69f0a9aba765618209b87df02dccd5aa57758cec
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-03 18:32:28 +0600

    FooterOne add

--- Master/src/app/index-1/page.jsx
+++ Master/src/app/index-1/page.jsx
@@ -2,10 +2,12 @@ import AboutOne from "@/components/AboutOne";
 import BannerOne from "@/components/BannerOne";
 import BlogOne from "@/components/BlogOne";
 import BrandOne from "@/components/BrandOne";
+import CertificateOne from "@/components/CertificateOne";
 import ChooseUsOne from "@/components/ChooseUsOne";
 import CounterOne from "@/components/CounterOne";
 import ExploreCourseOne from "@/components/ExploreCourseOne";
 import FeaturesOne from "@/components/FeaturesOne";
+import FooterOne from "@/components/FooterOne";
 import HeaderOne from "@/components/HeaderOne";
 import InstructorOne from "@/components/InstructorOne";
 import TestimonialsOne from "@/components/TestimonialsOne";
@@ -51,6 +53,12 @@ const page = () => {
 
       {/* BlogOne */}
       <BlogOne />
+
+      {/* CertificateOne */}
+      <CertificateOne />
+
+      {/* FooterOne */}
+      <FooterOne />
     </>
   );
 };
