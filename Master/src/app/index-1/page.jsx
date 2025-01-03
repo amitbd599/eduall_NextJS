5df3db3990842fd85a9505dd1f46281506e74a3d
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-03 11:21:11 +0600

    InstructorOne add

--- Master/src/app/index-1/page.jsx
+++ Master/src/app/index-1/page.jsx
@@ -4,11 +4,12 @@ import BrandOne from "@/components/BrandOne";
 import ExploreCourseOne from "@/components/ExploreCourseOne";
 import FeaturesOne from "@/components/FeaturesOne";
 import HeaderOne from "@/components/HeaderOne";
+import InstructorOne from "@/components/InstructorOne";
 
 export const metadata = {
-  title: "Digital Market Place NEXT Js Template",
+  title: "EdullAll - LMS, Tutors, Education & Online Course NEXT JS Template",
   description:
-    "DpMarket – Digital Products Marketplace NEXT JS Template – A versatile and meticulously designed set of templates crafted to elevate your Digital Products Marketplace content and experiences.",
+    "EduAll is a comprehensive and modern NEXT JS template designed for online education platforms, learning management systems (LMS), tutors, educational institutions, and online courses. It’s the perfect solution for creating an engaging and interactive online learning experience for students, educators, and institutions. Whether you’re offering online courses, running a tutoring platform, or managing an educational website, EduAll provides the tools to help you succeed. This template is tailored to meet the needs of educators, administrators, and students, providing a seamless and engaging user experience.",
 };
 
 const page = () => {
@@ -31,6 +32,9 @@ const page = () => {
 
       {/* AboutOne */}
       <AboutOne />
+
+      {/* InstructorOne */}
+      <InstructorOne />
     </>
   );
 };
