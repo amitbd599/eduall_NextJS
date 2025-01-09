1b8c540e3d28eba8c88c5ec8b93d46a93ebc79b4
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-09 10:35:33 +0600

    Create loading.js

---
+++ Master/src/app/loading.js
@@ -0,0 +1,8 @@
+// app/loading.js
+export default function Loading() {
+  return (
+    <div className='preloader'>
+      <img src='assets/images/icons/preloader.gif' alt='' />
+    </div>
+  );
+}
